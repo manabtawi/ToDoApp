8d62d7460a7bff735fe78f9ec511094ffd92881d
Mohammad Anabtawi <m_anabtawi@hotmail.com>
2017-05-18 16:26:56 +0300

    Done flag set to False on creation

--- ToDoApp/Controllers/ToDoController.cs
+++ ToDoApp/Controllers/ToDoController.cs
@@ -85,7 +85,7 @@ namespace ToDoApp.Controllers
                 Created = DateTime.Now,
                 UserId = RequestContext.Principal.Identity.GetUserId(),
                 Notes = model.Notes,
-                Done = model.Done
+                Done = false //model.Done
             };
             db.ToDoes.Add(obj);
             await db.SaveChangesAsync();
